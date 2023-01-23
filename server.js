1acdfb33c9cf60eb0d9683e2a1095d17d42d412c
Youssef Nafaa <nafaayoussef@gmail.com>
2023-01-23 14:46:20 -0500

    create routes

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const port = 1234;
 
 const app = express();
 
+const usersRoute = require('./routes/users');
+
+app.use("/api/users", usersRoute);
+
+app.get("/api", (req, res) => {
+  res.send('Hello from root api');
+});
+
 
 app.listen(port, () => {
   console.log('Server listening at port: ' + port);
