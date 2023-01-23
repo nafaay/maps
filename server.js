9c7ee794f35e50e7270c5e17f4fa628d9cf4bc61
Youssef Nafaa <nafaayoussef@gmail.com>
2023-01-23 12:34:12 -0500

    create server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const morgan = require('morgan');
+const port = 1234;
+
+const app = express();
+
+
+app.listen(port, () => {
+  console.log('Server listening at port: ' + port);
+})
