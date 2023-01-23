4871c537b70b3802467040a2e52b81b060289719
Youssef Nafaa <nafaayoussef@gmail.com>
2023-01-23 14:54:57 -0500

    create other routes

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const port = 1234;
 const app = express();
 
 const usersRoute = require('./routes/users');
+const mapsRoute = require('./routes/maps');
+const markersRoute = require('./routes/markers');
+const favouritesRoute = require('./routes/favourites');
+
 
 app.use("/api/users", usersRoute);
+app.use("/api/maps", mapsRoute);
+app.use("/api/markers", markersRoute);
+app.use("/api/favourites", favouritesRoute);
+
 
 app.get("/api", (req, res) => {
   res.send('Hello from root api');
